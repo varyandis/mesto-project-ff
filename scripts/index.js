6553d6cf49b743cd46a079e7b08133b36bb312c4
varya <varyandis@yandex.ru>
2024-02-24 10:16:01 +0300

    24.02.2024

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const userTemplate = document.querySelector("#card-template").content;
+/* const userTemplate = document.querySelector("#card-template").content;
 const usersOnline = document.querySelector(".places__list");
 
 initialCards.forEach(function (cardData) {
@@ -7,6 +7,7 @@ initialCards.forEach(function (cardData) {
     .cloneNode(true);
   userElement.querySelector(".card__image").src = cardData.link;
   userElement.querySelector(".card__title").textContent = cardData.name;
+  userElement.querySelector(".card__image").setAttribute('alt', cardData.name);
   usersOnline.append(userElement);
 });
 
@@ -16,3 +17,37 @@ deleteButtons.forEach(function (button) {
     event.target.closest(".places__item").remove();
   });
 });
+ */
+const userTemplate = document.querySelector("#card-template").content;
+const usersOnline = document.querySelector(".places__list");
+
+// @todo: Функция создания карточки
+function createCard(data, onDelete){
+  const cardElement = userTemplate.querySelector('.places__item').cloneNode(true); 
+  //устанавливаем данные карточки и обработчик клика по корзинке удаления
+  //...
+  cardElement.querySelector(".card__image").src = data.link;
+  cardElement.querySelector(".card__title").textContent = data.name;
+  cardElement.querySelector(".card__image").setAttribute('alt', data.name);
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", function () {
+    onDelete(cardElement);
+  });
+
+  return cardElement;
+}
+
+function handleDeleteCard(cardElement) {
+  cardElement.remove();
+}
+
+function renderCard(cardElement) {
+  usersOnline.append(cardElement);
+}
+
+initialCards.forEach((cardData) => { 
+  const card = createCard(cardData, handleDeleteCard);
+  renderCard(card);
+});
+
